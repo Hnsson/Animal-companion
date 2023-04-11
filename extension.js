25474992d5397608f4d093f7106b59531e725c11
Emil Hansson <89981140+Hnsson@users.noreply.github.com>
2023-04-12 00:16:06 +0200

    Add files via upload
    Added dynamic changing of the image with postMessage() without re-rendering the html

--- extension.js
+++ extension.js
@@ -23,20 +23,31 @@ function activate(context) {
 			} // Webview options. More on these later.
 		);
 
-		const updateWebView = () => {
-			let index = count++ % paths.length;
+		panel.webview.html = getWebViewContent();
+		let index = 0;
 
-			let demon_path = vscode.Uri.joinPath(context.extensionUri, paths[index]["path"])
+		const image_path = vscode.Uri.joinPath(context.extensionUri, paths[index]["path"])
+		console.log(image_path);
+		const image_src = panel.webview.asWebviewUri(image_path);
+		panel.webview.postMessage({
+			type: 'new-demon',
+			src: image_src.toString(),
+			width: paths[index]["width"]
+		});
 
-			const demonGif = panel.webview.asWebviewUri(demon_path);
-			panel.webview.html = getWebViewContent(demonGif, paths[index]["width"]);
-		}
+		setInterval(() => {
+			index = (index + 1) % paths.length;
 
-		updateWebView();
-		setInterval(updateWebView, 1000);
+			console.log(index);
+			const newImagePath = vscode.Uri.joinPath(context.extensionUri, paths[index]["path"])
+			const newImageSrc = panel.webview.asWebviewUri(newImagePath);
+			panel.webview.postMessage({
+				type: 'new-demon',
+				src: newImageSrc.toString(),
+				width: paths[index]["width"]
+			});
+		},10000)
 
-		panel.webview.postMessage({command: 'walking'});
-		
 
 		panel.onDidDispose(() => {
 			panel = undefined;
@@ -46,7 +57,7 @@ function activate(context) {
 	}));
 }
 
-function getWebViewContent(demon_path, demon_size) {
+function getWebViewContent() {
 	return `<!DOCTYPE html>
 <html lang="en">
 <head>
@@ -55,58 +66,71 @@ function getWebViewContent(demon_path, demon_size) {
     <title>Cat Coding</title>
 
 	<style>
+		body {
+			overflow: hidden;
+		}
+
 		img {
+			width: 100%;
 			image-rendering: pixelated;
 			image-rendering: -moz-crisp-edges;
 			image-rendering: crisp-edges;
 			position:absolute;
 			bottom:0;
-			left:50%;
+			left:0%;
 			transform:translate(-50%);
 		}
 	</style>
 </head>
 <body>
-    <img id="demon" src="${demon_path}" width="${demon_size}" />
+    <img id="demon" src="" width="100" />
 
 	<script>
+	const vscode = acquireVsCodeApi();
 		let state = "walking";
 		let direction = 1;
 		let x = 0;
 		let speed = 10;
 
-		window.addEventListener('message', even => {
-			switch(event.data.command) {
-				case "idle":
-					state = "idle"
+		window.addEventListener('message', event => {
+			const message = event.data;
+
+			switch(message.type) {
+				case 'new-demon':
+					const img = document.getElementById('demon');
+					img.src = message.src;
+					img.style.maxWidth = message.width + 'px';
 					break;
-				case "walking":
-					state = "walking"
+				case 'walking':
+					state = 'walking';
+					break;
+				case 'idle':
+					state = 'idle';
 					break;
 				default:
 					break;
 			}
-		})
+		});
 
-		// setInterval(() => {
-		// 	if(state == "idle") {
-		// 		console.log("idle");
-		// 	}
-		// 	if(state == "walking") {
-		// 		console.log("walking");
-		// 		let rect = document.getElementById("demon").getBoundingClientRect();
-		// 		if(rect.left <= 0) {
-		// 			direction = 1;
-		// 		}
-		// 		if(rect.right >= (window.innerWidth || document.documentElement.clientWidth)) {
-		// 			direction = -1;
-		// 		}
-	
-		// 		x += speed * direction;
-		// 		document.getElementById("demon").style.left = x + 'px';
-		// 	}
-		// }, 300)
-		
+		setInterval(() => {
+			if(state == "idle") {
+				console.log("idle");
+			}
+			if(state == "walking") {
+				console.log("walking");
+				let rect = document.getElementById("demon").getBoundingClientRect();
+				if(rect.left <= 0) {
+					direction = 1;
+				}
+				if(rect.right >= (window.innerWidth || document.documentElement.clientWidth)) {
+					direction = -1;
+				}
+
+				x += speed * direction;
+				document.getElementById("demon").style.left = x + 'px';
+			}
+		}, 100)
+
 	</script>
 </body>
 </html>`;
